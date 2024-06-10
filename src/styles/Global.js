679b5d092e858ace06b3b55f41804139dbda41ec
BrunoH300 Freitas <brunoziycabr959@gmail.com>
2024-06-10 12:29:40 +0000

    Card no centro da tela

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -5,5 +5,19 @@ export  const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: "Overpass", sans-serif;
+ }
+
+ :root {
+    font-size: 62.5%;
+ }
+
+ body{
+    font-size: 1.6rem;
+    background: ${props => props.theme.HomeBackground};
+    height: 100sv;
+    display: flex;
+    align-items: center;
+    justify-content: center;
  }
 `
