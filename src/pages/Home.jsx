7d699e77cffd3d9b4d9bb58a7c1dff5f8a5c1f88
BrunoH300 Freitas <brunoziycabr959@gmail.com>
2024-06-11 02:40:03 +0000

    Finalizando projeto FeedBack

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,52 @@
 import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "./Home.styles";
 import iconStarImg from "../assets/icon-star.svg";
 import thankyouImg from "../assets/illustration-thank-you.svg";
-
-let aparecerResultado = true
+import { useState } from "react";
 
 export function Home() {
+  const [mostrarResultado, setMostarResultado] = useState(false);
+  const [feedbackNote, setFeedbackNote] = useState(0);
+
+  function handleFeedbackButtonClick(event) {
+    const feedback = Number(event.target.innerText);
+    setFeedbackNote(feedback);
+  }
+
+  function handleSubmit() {
+    if (feedbackNote === 0) return;
+    setMostarResultado(true);
+  }
+
+  console.log(feedbackNote);
+
   return (
-   aparecerResultado === false ? (
-    <CardContainer>
-    <IconContainer>
-      <img src={iconStarImg} alt="estrela" />
-    </IconContainer>
-    <h1>Como foi o atendimento?</h1>
-    <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento.</p> 
+    mostrarResultado === false ? (
+      <CardContainer>
+        <IconContainer>
+          <img src={iconStarImg} alt="estrela" />
+        </IconContainer>
+        <h1>Como foi o atendimento?</h1>
+        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento.</p> 
 
-    <ButtonContainer>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-    </ButtonContainer>
+        <ButtonContainer>
+          <button onClick={handleFeedbackButtonClick}>1</button>
+          <button onClick={handleFeedbackButtonClick}>2</button>
+          <button onClick={handleFeedbackButtonClick}>3</button>
+          <button onClick={handleFeedbackButtonClick}>4</button>
+          <button onClick={handleFeedbackButtonClick}>5</button>
+        </ButtonContainer>
 
-    <button>Enviar</button>
-  </CardContainer>
-   
-  ) :(
-    <CardContainer>
-    <img src= {thankyouImg} alt="" />
-    <ResultContainer>
-      <p>Você selecionou 4 de 5</p>
-    </ResultContainer>
-    <h1>Obrigado!</h1>
-    <p>Agradeçemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entar em contato!</p>
-    </CardContainer>
-   )
+        <button onClick={handleSubmit}>Enviar</button>
+      </CardContainer>
+    ) : (
+      <CardContainer>
+        <img src={thankyouImg} alt="" />
+        <ResultContainer>
+          <p>Você selecionou {feedbackNote} de 5</p>
+        </ResultContainer>
+        <h1>Obrigado!</h1>
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+      </CardContainer>
+    )
   );
 }
