38c12c90304e56daf56c32b47fa4a917557c1af7
BrunoH300 Freitas <brunoziycabr959@gmail.com>
2024-06-11 01:57:28 +0000

    estilização completa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 import { Home } from "./pages/Home"
-import {theme} from "./styles/theme"
+import { theme } from "./styles/theme"
 import { GlobalStyle } from "./styles/Global"
-
 
 const router = createBrowserRouter([
     {
@@ -14,10 +13,9 @@ const router = createBrowserRouter([
 
 export function App() {
   return (
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-        <GlobalStyle />
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,39 @@
-import { ButtonContainer, CardContainer } from "./Home.styles";
-import iconStarImg from "../assets/icon-star.svg"
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "./Home.styles";
+import iconStarImg from "../assets/icon-star.svg";
+import thankyouImg from "../assets/illustration-thank-you.svg";
+
+let aparecerResultado = true
 
 export function Home() {
   return (
+   aparecerResultado === false ? (
     <CardContainer>
-      <iconContainer>
-        <img src= {iconStarImg} alt="estrela" />
-      </iconContainer>
-      <h1>
-        Como foi o atendimento ?
-      </h1>
-      <p>
-        Conte-nos como foi nossa ajuda com sua solicitação, Agradecemos muito seu feedback para podermos melhorar nosso atendimento
-      </p> 
+    <IconContainer>
+      <img src={iconStarImg} alt="estrela" />
+    </IconContainer>
+    <h1>Como foi o atendimento?</h1>
+    <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento.</p> 
 
-      <ButtonContainer>
-     <button> 1 </button>
-     <button> 2 </button>
-     <button> 3 </button>
-     <button> 4 </button>
-     <button> 5 </button>
-      </ButtonContainer>
+    <ButtonContainer>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+    </ButtonContainer>
 
     <button>Enviar</button>
+  </CardContainer>
+   
+  ) :(
+    <CardContainer>
+    <img src= {thankyouImg} alt="" />
+    <ResultContainer>
+      <p>Você selecionou 4 de 5</p>
+    </ResultContainer>
+    <h1>Obrigado!</h1>
+    <p>Agradeçemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entar em contato!</p>
     </CardContainer>
-  )
+   )
+  );
 }
